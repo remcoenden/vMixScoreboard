5542c6f3978289b1ff8d5f9217ddc0be5a026601
Remco van den Enden <rvdenden99@gmail.com>
2021-08-05 21:33:16 +0200

    Write prototype function for getting actual data from the raw scoreboard output

--- Source/DataDisplay.py
+++ Source/DataDisplay.py
@@ -29,7 +29,7 @@ class DataDisplay:
     # if the first char doesn't match up.
     # The method will read until a '}' is received, indicating
     # the end of a message.
-    def read(self):
+    def readScoreboardData(self):
         data = ''
         while True:
             rawData = read_until('}')
@@ -38,8 +38,34 @@ class DataDisplay:
             if (rawData[0] == '{') and (rawData[-1:] == '}'):
                 data = rawData
                 break
-            rawData = ''
-            
-            
+            else:
+                rawData = ''
         
-            
+    # The whole raw data message should be 31 bytes long
+    # The messages containts both the time and score, long with some
+    # other random stuff that is not needed for this application
+    # Counting from the first byte received, starting at 0, the data
+    # is stored in the following bytes:
+    #     Minutes: 17 & 18
+    #     Seconds: 19 & 20
+    #     Home:    13 & 14  TODO: Needs to be checked
+    #     Guest:   15 & 16  TODO: Needs to be checked
+    # Most of these bytes are plain ASCII, with the exception of the
+    # second minute byte. A look-up tables needs to be used to read
+    # that one out.
+    def getScoreboardData(self, rawData):
+        d = dict()
+        
+        d['seconds'] = rawData[19:21]
+        d['home'] = rawData[13:15]
+        d['guest'] = rawData[15:17]
+        
+        # To get the second minutes digit, the last bit of the ASCII
+        # char needs to be fliped. This can be done with a XOR operation
+        # To do this, the string is converted to a ASCII int representetive
+        # which can be used for bitwise operations
+        temp = ord(rawData[18])
+        temp = chr(temp ^ 1)
+        d['minutes'] = rawData[17] + temp
+        return d
+    
